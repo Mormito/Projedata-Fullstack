bda824296ea6008c1087b6275664f4f651c18dfe
Mormito <guimormis@gmail.com>
2026-02-28 13:38:03 -0300

    Update now shows old data, but still doesn't refetch data :(

--- ProjedataFront/src/components/material/materialForm.tsx
+++ ProjedataFront/src/components/material/materialForm.tsx
@@ -13,16 +13,25 @@ import { CreateUpdateMaterial } from "./postMaterial"
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
-import type { FormUpdateProps } from "@/interfaces/Interfaces";
+import type { FormUpdateProps, Material } from "@/interfaces/Interfaces";
 
 
 export function FormMaterial({ option, id }: FormUpdateProps){
     const [name, setName] = useState("");
     const [quantity, setQuantity] = useState(0);
 
+    async function loadMaterialData() {
+      const res = await fetch(`http://localhost:8080/raw_material/${id}`)
+      const material: Material = await res.json();
+        if (material) {
+            setName(material.name);
+            setQuantity(material.quantity);
+        }
+    }
+
     return(
         <Dialog>
-            <DialogTrigger asChild>{option == true ? <Button className="w-full flex items-center gap-2 justify-center"><Plus />Create Material</Button> : <NotebookPen className="cursor-pointer hover:text-primary duration-200 ease-in-out"/>}</DialogTrigger>
+            <DialogTrigger asChild onClick={() => {id && loadMaterialData()}}>{option == true ? <Button className="w-full flex items-center gap-2 justify-center"><Plus />Create Material</Button> : <NotebookPen className="cursor-pointer hover:text-primary duration-200 ease-in-out"/>}</DialogTrigger>
 
             <DialogContent>
                 <DialogHeader>
@@ -33,12 +42,12 @@ export function FormMaterial({ option, id }: FormUpdateProps){
                      
                     <div className="flex flex-col gap-1">
                     <Label>Material name</Label>
-                    <Input placeholder="Material" id="name" onChange={(e) => setName(e.target.value)} required/>
+                    <Input placeholder="Material" id="name" value={name} onChange={(e) => setName(e.target.value)} required/>
                     </div>
 
                     <div className="flex flex-col gap-1">
                     <Label>Material quantity</Label>
-                    <Input type="number" placeholder="Example: 10" id="quantity" onChange={(e) => setQuantity(Number(e.target.value))} required/>
+                    <Input type="number" placeholder="Example: 10" id="quantity" value={quantity} onChange={(e) => setQuantity(Number(e.target.value))} required/>
                     </div>
 
                     <CreateUpdateMaterial url="raw_material" content="Raw Material" id={id} name={name} quantity={quantity}/>
--- ProjedataFront/src/components/product/productForm.tsx
+++ ProjedataFront/src/components/product/productForm.tsx
@@ -13,15 +13,24 @@ import { Label } from "../ui/label"
 import { Input } from "../ui/input"
 import { CreateUpdateProduct } from "./postProduct"
 import { useState } from "react"
-import type { FormUpdateProps } from "@/interfaces/Interfaces"
+import type { FormUpdateProps, Product } from "@/interfaces/Interfaces"
 
 export function FormProduct({ option, id }: FormUpdateProps){
     const [name, setName] = useState("");
     const [price, setPrice] = useState(0);
 
+    async function loadProductData() {
+      const res = await fetch(`http://localhost:8080/product/${id}`)
+      const product: Product = await res.json();
+        if (product) {
+            setName(product.name);
+            setPrice(product.price);
+        }
+    }
+
     return(
         <Dialog>
-            <DialogTrigger asChild>{option == true ? <Button className="w-full flex items-center gap-2 justify-center"><Plus />Create Product</Button> : <NotebookPen className="cursor-pointer hover:text-primary duration-200 ease-in-out"/>}</DialogTrigger>
+            <DialogTrigger asChild onClick={() => {id && loadProductData()}}>{option == true ? <Button className="w-full flex items-center gap-2 justify-center"><Plus />Create Product</Button> : <NotebookPen className="cursor-pointer hover:text-primary duration-200 ease-in-out"/>}</DialogTrigger>
 
             <DialogContent>
                 <DialogHeader>
@@ -35,12 +44,12 @@ export function FormProduct({ option, id }: FormUpdateProps){
                      
                     <div className="flex flex-col gap-1">
                     <Label>Product name</Label>
-                    <Input placeholder="Product" id="name" onChange={(e) => setName(e.target.value)} required/>
+                    <Input placeholder="Product" id="name" value={name} onChange={(e) => setName(e.target.value)} required/>
                     </div>
 
                     <div className="flex flex-col gap-1">
                     <Label>Product price</Label>
-                    <Input type="text" placeholder="Example: 12.50" id="price" onChange={(e) => setPrice(Number(e.target.value))} required/>
+                    <Input type="text" placeholder="Example: 12.50" value={price} id="price" onChange={(e) => setPrice(Number(e.target.value))} required/>
                     </div>
 
                     <CreateUpdateProduct url="product" content="Product" id={id} name={name} price={price} />
